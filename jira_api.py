1f538099b3e1ee53bae319b2c108cb38349cbbd5
mazitov <Timur.Mazitov@rtmis.ru>
2021-06-28 22:11:23 +0500

    Добавил получение задачи по API. Переместил jira.py в jira_classes из-за коллизии.

--- jira_api.py
+++ jira_api.py
@@ -1,13 +1,28 @@
 from jira import JIRA
 
-def jira_issues(user_login, password, jql,server="https://jira.is-mis.ru", max_result = 100):
-    jira_connection = JIRA(server=server,basic_auth=(user_login, password))
+
+def jira_issues(
+    user_login, password, jql, server="https://jira.is-mis.ru", max_result=100
+):
+    """
+    Возвращает список задач по запросу
+
+    :param user_login: логин
+    :param password: пароль
+    :param jql: JQL запрос
+    :param server: адрес сервера
+    :param max_result: максимальное количество задач
+    :return: список словарей
+    """
+    jira_connection = JIRA(server=server, basic_auth=(user_login, password))
     issues = jira_connection.search_issues(maxResults=max_result, jql_str=jql)
 
     return [
-            {'id': issue.raw['id'],
-             'key': issue.raw['key'],
-             'branches': issue.raw['fields']['customfield_12501'],
-             'templates': issue.raw['fields']['customfield_11983']
-             } for issue in issues
-            ]
+        {
+            "id": issue.raw["id"],
+            "key": issue.raw["key"],
+            "branches": issue.raw["fields"]["customfield_12501"],
+            "templates": issue.raw["fields"]["customfield_11983"],
+        }
+        for issue in issues
+    ]
